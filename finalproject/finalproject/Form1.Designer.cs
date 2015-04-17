de8b3de47ba201e688757194f57ec55290079968
Sean Patrick Lyons <itsseanl@gmail.com>
2015-04-16 23:12:25 -0400

    First Commit

---
+++ finalproject/finalproject/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wikiBrowser = new System.Windows.Forms.WebBrowser();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnSaveNotes = new System.Windows.Forms.Button();
+            this.txtBox = new System.Windows.Forms.RichTextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.btnDeletePage = new System.Windows.Forms.Button();
+            this.txtDeleteBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLinkName = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // wikiBrowser
+            // 
+            this.wikiBrowser.Location = new System.Drawing.Point(13, 5);
+            this.wikiBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wikiBrowser.Name = "wikiBrowser";
+            this.wikiBrowser.Size = new System.Drawing.Size(879, 441);
+            this.wikiBrowser.TabIndex = 0;
+            this.wikiBrowser.Url = new System.Uri("http://en.wikipedia.org/wiki/Special:Random", System.UriKind.Absolute);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(898, 5);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(220, 23);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Get Random Page";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(898, 34);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(220, 412);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.linkLabel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(212, 386);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Saved Pages";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(6, 3);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(174, 13);
+            this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Invisible, reference pt for other links";
+            this.linkLabel1.Visible = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btnSaveNotes);
+            this.tabPage2.Controls.Add(this.txtBox);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(212, 386);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Notes";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnSaveNotes
+            // 
+            this.btnSaveNotes.Location = new System.Drawing.Point(65, 330);
+            this.btnSaveNotes.Name = "btnSaveNotes";
+            this.btnSaveNotes.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveNotes.TabIndex = 1;
+            this.btnSaveNotes.Text = "Save Notes";
+            this.btnSaveNotes.UseVisualStyleBackColor = true;
+            this.btnSaveNotes.Click += new System.EventHandler(this.btnSaveNotes_Click);
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(3, 3);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(206, 321);
+            this.txtBox.TabIndex = 0;
+            this.txtBox.Text = "";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tabPage3.Controls.Add(this.btnDeletePage);
+            this.tabPage3.Controls.Add(this.txtDeleteBox);
+            this.tabPage3.Controls.Add(this.label2);
+            this.tabPage3.Controls.Add(this.label1);
+            this.tabPage3.Controls.Add(this.txtLinkName);
+            this.tabPage3.Controls.Add(this.btnSave);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(212, 386);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Save/Delete";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // btnDeletePage
+            // 
+            this.btnDeletePage.Location = new System.Drawing.Point(64, 126);
+            this.btnDeletePage.Name = "btnDeletePage";
+            this.btnDeletePage.Size = new System.Drawing.Size(76, 23);
+            this.btnDeletePage.TabIndex = 8;
+            this.btnDeletePage.Text = "Delete Page";
+            this.btnDeletePage.UseVisualStyleBackColor = true;
+            this.btnDeletePage.Click += new System.EventHandler(this.btnDeletePage_Click);
+            // 
+            // txtDeleteBox
+            // 
+            this.txtDeleteBox.Location = new System.Drawing.Point(4, 100);
+            this.txtDeleteBox.Name = "txtDeleteBox";
+            this.txtDeleteBox.Size = new System.Drawing.Size(200, 20);
+            this.txtDeleteBox.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Search for link to delete (search by name):";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Add a name to your saved link:";
+            // 
+            // txtLinkName
+            // 
+            this.txtLinkName.Location = new System.Drawing.Point(6, 24);
+            this.txtLinkName.Name = "txtLinkName";
+            this.txtLinkName.Size = new System.Drawing.Size(200, 20);
+            this.txtLinkName.TabIndex = 4;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(64, 47);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(76, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save Page";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 458);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.wikiBrowser);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "WikiPro";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser wikiBrowser;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.RichTextBox txtBox;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLinkName;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button btnSaveNotes;
+        private System.Windows.Forms.Button btnDeletePage;
+        private System.Windows.Forms.TextBox txtDeleteBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ finalproject/finalproject/Form1.cs
@@ -0,0 +1,244 @@
+﻿//Sean Lyons
+//12/5/14
+//Final Project - WikiPro - Search through wikipedia articles, find random ones, save links with custom names and notes
+//utilizes search techniques, serialization, array lists, regular lsts, multithreading
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    //allows information to be serialized
+    [Serializable]
+    public partial class Form1 : Form
+    {
+        //formatter for serialization
+        BinaryFormatter formatter = new BinaryFormatter();
+        //array lists for saved data to be saved from and loaded into
+        ArrayList savedLinkName = new ArrayList();
+        ArrayList savedLinks = new ArrayList();
+        ArrayList savedNotes = new ArrayList();
+        //list so that it can be assigned to linkLabel type
+        List<LinkLabel> linkLabels = new List<LinkLabel>();
+        //threads to open and save all data simultaneously
+        static Thread openThreadOne;
+        static Thread openThreadTwo;
+        static Thread openThreadThree;
+        static Thread saveThreadOne;
+        static Thread saveThreadTwo;
+        static Thread saveThreadThree;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //initialize and start open/read threads for saved data
+            openThreadOne = new Thread(new ThreadStart(OpenThreadOne));
+            openThreadTwo = new Thread(new ThreadStart(OpenThreadTwo));
+            openThreadThree = new Thread(new ThreadStart(OpenThreadThree));
+            openThreadOne.Start();
+            openThreadTwo.Start();
+            openThreadThree.Start();
+            //sleep allows threads to complete fully before link labels are created
+            Thread.Sleep(100);
+            //calls method responsible for creating/recreating link labels
+            LinkLabelCreate();
+        }
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        //methods responsible for loading saved data. Seperated for multithreading.
+        //try catch in case the files don't exist/can't be found
+        //open, deserialize, close streams
+        // try/catch in case it's first time using program or files cannot be found
+        private void OpenThreadOne()
+        {
+            try
+            {
+                Stream input = File.OpenRead("savedLinkName.txt");
+                savedLinkName = (ArrayList)formatter.Deserialize(input);
+                input.Close();
+            }
+            catch
+            {
+                return;
+            }
+        }
+        private void OpenThreadTwo()
+        {
+            try
+            {
+                Stream input2 = File.OpenRead("savedLinks.txt");
+                savedLinks = (ArrayList)formatter.Deserialize(input2);
+                input2.Close();
+            }
+            catch
+            {
+                return;
+            }
+        }
+        private void OpenThreadThree()
+        {
+            try
+            {
+                Stream input3 = File.OpenRead("savedNotes.txt");
+                savedNotes = (ArrayList)formatter.Deserialize(input3);
+                input3.Close();
+            }
+            catch
+            {
+                return;
+            }
+        }
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        private void linkClick(object sender, EventArgs e)
+        {
+            //get name of link from link label being clicked
+            string linkName = Convert.ToString(sender.GetType().GetProperty("Text").GetValue(sender, null));
+            string linkNameMatch = "";
+            int i = 0;
+            //find array list position for link so that link notes can be opened
+            while (linkNameMatch != Convert.ToString(linkLabels[i].Text))
+            {
+                //check each saved link name against the link that was clicked to find which link it is
+                linkNameMatch = savedLinkName[i].ToString();
+                if (linkNameMatch != linkName)
+                    i++;
+            }
+            //set textbox to saved notes
+            txtBox.Text = savedNotes[i].ToString();
+            //open saved link
+            Uri wikiBrowserURL = new Uri(savedLinks[i].ToString());
+            wikiBrowser.Navigate(wikiBrowserURL);
+        }
+
+        //method to refresh web browser with new wiki article
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            Uri wikiBrowserURL =  new Uri("http://en.wikipedia.org/wiki/Special:Random");
+            wikiBrowser.Navigate(wikiBrowserURL);
+            //remove notes from last page if there were any
+            txtBox.Text = "";
+        }
+
+        //method for saving page name and link, along with notes
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            //assign linkName, link, and notes to variables
+            string linkName = txtLinkName.Text.ToString();
+            string link = wikiBrowser.Url.ToString();
+            string notes = txtBox.Text;
+            //add to respective strings
+            savedLinkName.Add(linkName);
+            savedLinks.Add(link);
+            savedNotes.Add(notes);
+
+            //calls method responsible for creating/recreating link labels
+            LinkLabelCreate();
+            
+            //initialize and start threads to save data
+            saveThreadOne = new Thread(new ThreadStart(SaveThreadOne));
+            saveThreadTwo = new Thread(new ThreadStart(SaveThreadTwo));
+            saveThreadThree = new Thread(new ThreadStart(SaveThreadThree));
+            saveThreadOne.Start();
+            saveThreadTwo.Start();
+            saveThreadThree.Start();  
+        }
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        //methods used for saving, seperated for multithreading.
+        //create file, save specified array list, close
+        public void SaveThreadOne()
+        {
+            Stream output = File.Create("savedLinkName.txt");
+            formatter.Serialize(output, savedLinkName);
+            output.Close();
+        }
+        public void SaveThreadTwo()
+        {
+            Stream output2 = File.Create("savedLinks.txt");
+            formatter.Serialize(output2, savedLinks);
+            output2.Close();
+        }
+        public void SaveThreadThree()
+        {
+            Stream output3 = File.Create("savedNotes.txt");
+            formatter.Serialize(output3, savedNotes);
+            output3.Close();
+        }
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        //method for button to save notes on an already saved link
+        private void btnSaveNotes_Click(object sender, EventArgs e)
+        {
+            string linkName = wikiBrowser.Url.ToString();
+            //while loop to get position of link, which will be the same as notes
+            int i = 0;
+            while (savedLinks[i].ToString() != linkName)
+            {
+                //increment up until position of link in array list is found
+                i++;
+            }
+            
+
+            //save notes to correct position
+            savedNotes[i] = txtBox.Text;
+            //delete old array list, recreate with new notes
+            File.Delete("savedNotes.txt");
+            Stream output = File.Create("savedNotes.txt");
+            formatter.Serialize(output, savedNotes);
+            output.Close();
+        }
+
+        private void btnDeletePage_Click(object sender, EventArgs e)
+        {
+            //get inputted link to be deleted
+            string deleteLink = txtDeleteBox.Text;
+            int z = 0;
+            while (savedLinkName[z].ToString() != deleteLink)
+            {
+                z++;
+            }
+            //remove all associated link information
+            savedLinkName.RemoveAt(z);
+            savedLinks.RemoveAt(z);
+            savedNotes.RemoveAt(z);
+
+            //initialize and start threads to save data
+            saveThreadOne = new Thread(new ThreadStart(SaveThreadOne));
+            saveThreadTwo = new Thread(new ThreadStart(SaveThreadTwo));
+            saveThreadThree = new Thread(new ThreadStart(SaveThreadThree));
+            saveThreadOne.Start();
+            saveThreadTwo.Start();
+            saveThreadThree.Start();
+            //restart application, couldn't figure out any other way to remove link labels for deleted links
+            Application.Restart();            
+        }
+
+        public void LinkLabelCreate()
+        {
+            //recreate link list with added label
+            for (int i = 0; i < savedLinks.Count; i++)
+            {
+                //set label properties
+                linkLabels.Add(
+                     new LinkLabel()
+                     {
+                         Name = "link" + i.ToString()
+                     });
+                linkLabels[i].Text = savedLinkName[i].ToString();
+                linkLabels[i].Click += new EventHandler(linkClick);
+                if (i == 0)
+                    linkLabels[i].Location = new Point(linkLabel1.Location.X, linkLabel1.Location.Y + 20);
+                else
+                    linkLabels[i].Location = new Point(linkLabels[i - 1].Location.X, linkLabels[i - 1].Location.Y + 22);
+                //add controls to page
+                tabPage1.Controls.Add(linkLabels[i]);
+            }
+        }
+    }
+}
